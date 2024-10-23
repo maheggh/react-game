0af9a1a55d61c937fe7c960339dc20163c13494b
maheggh <111765195+maheggh@users.noreply.github.com>
2024-10-23 20:38:35 +0200

    ready for railway

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 // Load environment variables
 dotenv.config();
 
 // Import routes
-const userRoutes = require('./routes/userRoutes'); 
+const userRoutes = require('./routes/userRoutes');
 const carRoutes = require('./routes/carRoutes');
 const carRacesRoutes = require('./routes/carRacesRoutes');
 const carTheftRoutes = require('./routes/carTheftRoutes');
@@ -21,14 +23,18 @@ const playerRoutes = require('./routes/playerRoutes');
 
 const app = express();
 
-// Enable CORS
 app.use(cors());
-
-// Middleware to parse JSON
 app.use(express.json());
 
-// Use routes
-app.use('/api/users', userRoutes); 
+const registrationLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: { success: false, message: 'Too many accounts created from this IP, please try again later.' },
+});
+
+app.use('/api/users/register', registrationLimiter);
+
+app.use('/api/users', userRoutes);
 app.use('/api/cars', carRoutes);
 app.use('/api/carraces', carRacesRoutes);
 app.use('/api/cartheft', carTheftRoutes);
@@ -40,7 +46,6 @@ app.use('/api/assassination', assassinationRoutes);
 app.use('/api/spin', gamblingRoutes);
 app.use('/api/players', playerRoutes);
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -48,29 +53,16 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => {
   console.error('MongoDB connection error:', err);
-  process.exit(1); // Ensure the server doesn't start if DB connection fails
+  process.exit(1);
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error('Server Error:', err);
-  res.status(500).json({ message: 'Internal server error' });
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const rateLimit = require('express-rate-limit');
-
-// Define rate limiting rules
-const registrationLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 registration requests per windowMs
-  message: { success: false, message: 'Too many accounts created from this IP, please try again later.' },
-});
-
-// Apply the rate limiter to your registration route
-app.use('/api/users/register', registrationLimiter);
